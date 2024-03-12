648b4bd4cd5c1ffdc8173c6aa92da22a3b4bb660
Margarita <pitinovamargo@gmail.com>
2024-03-13 00:12:47 +0100

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Margarita Pitinova on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
